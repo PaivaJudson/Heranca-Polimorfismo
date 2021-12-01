e8f4c913bd5aedd2c7ffe9fa91fb139446d12041
Judson Paiva <judson.paiva@hotmail.com>
2021-12-01 12:17:03 +0100

    Modificadores de acesso

--- src/paiva/judson/app/App.java
+++ src/paiva/judson/app/App.java
@@ -1,14 +1,26 @@
 package paiva.judson.app;
 
+import paiva.judson.app.testmodificadores.Bolseiro;
+
 public class App {
     public static void main(String[] args) {
 
         Pessoa p1 = new Pessoa();
-        Pessoa p2 = new Pessoa();
+
 
         p1.nome = "Judson Paiva";
         p1.idade = 27;
-
         p1.mostrarDados();
+
+
+        Pessoa p2 = new Pessoa();
+        p2.mostrarDados();
+
+        Pessoa p3 = new Pessoa("Leona José", 24, 250.0);
+        p3.mostrarDados();
+
+        Bolseiro bolseiro = new Bolseiro();
+
+
     }
 }
--- src/paiva/judson/app/Pessoa.java
+++ src/paiva/judson/app/Pessoa.java
@@ -1,12 +1,18 @@
 package paiva.judson.app;
 
 public class Pessoa {
-    String nome;
-    int idade;
+    protected String nome;
+    public int idade;
+    protected double subsídio;
 
 
     public Pessoa() {
         this.nome = "Sem nome";
+        this.idade = 0;
+    }
+
+    public Pessoa(String nome){
+        this.nome = nome;
         this.idade = 0;
     }
 
@@ -15,9 +21,13 @@ public class Pessoa {
         this.idade = idade;
     }
 
-
+    public Pessoa(String nome, int idade, double subsídio) {
+        this.nome = nome;
+        this.idade = idade;
+        this.subsídio = subsídio;
+    }
 
     void mostrarDados(){
-        System.out.printf("Nome: %s tem %d \n", this.nome, this.idade);
+        System.out.printf("Nome: %s tem %d de idade e aufere %f kz de subsídio \n", this.nome, this.idade, this.subsídio);
     }
 }
